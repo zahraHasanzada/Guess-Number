4ef33bfdfd0c2d135e644c2e3e520a7883379730
zahraHasanzada <116168664+zahraHasanzada@users.noreply.github.com>
2023-12-04 04:14:33 -0600

    Add files via upload

---
+++ ScriptGuess.js
@@ -0,0 +1,16 @@
+// "use strict"
+
+// document.querySelector(".number").textContent= ;
+// document.querySelector(".guess").value=12;
+// const secretNumber = Math.trunc(Math.random()*20 +1)
+// document.querySelector(".number").textContent= secretNumber;
+
+// document.querySelector(".cheak").addEventListener(click,function(){
+//     const guessNumber =Number(document.querySelector(".guess").value);
+//     console.log(guessNumber)
+//     if(!guessNumber){
+//         document.querySelector('.message').textContent = "No Number";
+//     }else if (guessNumber > secretNumber){
+//         document.querySelector('.message').textContent = "high"; 
+//     }
+// })
